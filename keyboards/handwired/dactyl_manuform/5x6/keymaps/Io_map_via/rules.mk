d427621e148864a65ffda10d9d20ab722dcd402b
IoakeimSogiakas <ioakeim.sogiakas@gmail.com>
2023-07-07 21:01:41 +0300

    VIA set up

--- keyboards/handwired/dactyl_manuform/5x6/keymaps/Io_map_via/rules.mk
+++ keyboards/handwired/dactyl_manuform/5x6/keymaps/Io_map_via/rules.mk
@@ -1,7 +1,13 @@
-# Build Options
-#   change yes to no to disable
+# MCU name
+MCU = atmega32u4
+
+# Bootloader selection
+BOOTLOADER = caterina
+
+# Build Options 
+# Change yes to no to disable
 #
-BOOTMAGIC_ENABLE = no       # Enable Bootmagic Lite
+BOOTMAGIC_ENABLE = yes      # Enable Bootmagic Lite
 MOUSEKEY_ENABLE = yes       # Mouse keys
 EXTRAKEY_ENABLE = yes       # Audio control and System control
 CONSOLE_ENABLE = no         # Console for debug
@@ -10,4 +16,7 @@ NKRO_ENABLE = no            # Enable N-Key Rollover
 BACKLIGHT_ENABLE = no       # Enable keyboard backlight functionality
 RGBLIGHT_ENABLE = no        # Enable keyboard RGB underglow
 AUDIO_ENABLE = no           # Audio output
-SPLIT_KEYBOARD = yes
+SPLIT_KEYBOARD = yes		# Split
+# TAP_DANCE_ENABLE = yes		# Tap Dance	
+# COMBO_ENABLE = yes 			# Combo switches
+VIA_ENABLE = yes
